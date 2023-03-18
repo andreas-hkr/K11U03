8538e028af2c87ac8598e4fe4b501d4398a7a84b
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 09:38:56 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // - Läs in tal och lagra i en array (double)
+        // - Skapa en metod som tar en array som parameter och som returnerar
+        //   ifall arrayen är sorterad eller ej (boolean)
+
+
+    }
+}
